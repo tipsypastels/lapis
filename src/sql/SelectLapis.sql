c2862fd3400ae233e857600e6bc00d37faf90e4c
gsanghera08 <57701090+gsanghera08@users.noreply.github.com>
2019-11-25 21:09:49 -0800

    Update SelectLapis.sql

--- src/sql/SelectLapis.sql
+++ src/sql/SelectLapis.sql
@@ -1,7 +1,10 @@
 select name from Artifacts;
-select name, color, smell from Ingredients where status = 'rare';
-select A.name from Artifacts A, Contains C where A.id = C.artifactID and safetyLevel = 'safe';
-
+select name, color, smell from Ingredients where status = "rare";
+select A.name from Artifacts A, Contains C where A.id = C.artifactID and safetyLevel = "safe";
+select A.name from Artifacts A where not exists (select U.id from User where not exists (select T.userID from AuthoredBy));
 select status, count(*) from Ingredients group by status;
 select count(*) from Craftable;
 select name, price from Artifacts where price >= (select avg(price) from Artifacts);
+delete from Artifacts where name like ""%ring%"";    ---needs cascade delete from schema
+update TestersHave set address = "4589 146B street, Surrey" where name = "Dax Swift";
+
